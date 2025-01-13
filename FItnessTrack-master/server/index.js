e8bc82263d036d97a8426ea8e778438907a642b7
ruturajkhalkar <ruturajkhalkar@gmail.com>
2025-01-13 22:49:46 +0530

    new commit

--- FItnessTrack-master/server/index.js
+++ FItnessTrack-master/server/index.js
@@ -10,12 +10,12 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
-
 app.use("/api/user/", UserRoutes);
 // error handler
+
 app.use((err, req, res, next) => {
   const status = err.status || 500;
-  const message = err.message || "Something went wrong";
+  const message = err.message || "Something Went wrong";
   return res.status(status).json({
     success: false,
     status,
